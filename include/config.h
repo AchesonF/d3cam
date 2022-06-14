d61555260ec3dea45ee632963415558a16f1a150
htrfid <htrfid@htrfid.com>
2022-06-14 13:55:19 +0800

    fix cmake project name
    Signed-off-by: htrfid <htrfid@htrfid.com>

--- include/config.h
+++ include/config.h
@@ -5,10 +5,13 @@
 extern "C" {
 #endif
 
-#define CONFIG_APP_NAME			D3CAM_NAME
-#define SOFTWARE_VERSION		D3CAM_VERSION
+
+#define CONFIG_APP_NAME			PROJECT_UPPER_NAME
+#define SOFTWARE_VERSION		PROJECT_VERSION
 
 #define SOFT_BUILDTIME			(__DATE__", "__TIME__)	///< 程序编译时间
+#define COMPILER_VERSION		__VERSION__
+
 
 #if (BUILD_TYPE_DEBUG==1)
   #define CONFIG_DEBUG 0
